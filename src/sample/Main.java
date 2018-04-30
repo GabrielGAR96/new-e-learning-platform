6aff8672b4566f1564475182caa9fa123a15ba96
Leonardo Costa <leobarroscosta76@gmail.com>
2018-04-30 00:01:52 -0300

    anchorpane purple wrap

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("painelPrincipal.fxml"));
         primaryStage.setTitle("New E-Learning");
-        primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.setScene(new Scene(root, 910, 605));
 
 
         primaryStage.show();
